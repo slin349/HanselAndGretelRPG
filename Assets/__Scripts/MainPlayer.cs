0b51e4754ab24fe854a676a33687a2a514db6c2f
Gareth <Gareth.kh.lau@gmail.com>
2020-02-10 16:19:44 -0500

    Added main player singleton

---
+++ Assets/__Scripts/MainPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+    public static MainPlayer S = null;
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }       
+        else
+        {
+            Debug.Log("Singleton already exists!");
+        }
+    }
+
+}
