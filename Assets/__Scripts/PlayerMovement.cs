8f474198f7e7f8de3d27400875fcf2d1d614497f
Gareth <Gareth.kh.lau@gmail.com>
2020-03-05 20:07:35 -0500

    Developing main character functionality
    In this commit, the main player character was replaced and player movement animations and attack animations were added. Player attack logic was also that allows the main player to inflict damage to enemies. Basic idle and death animations were added to enemies.

--- Assets/__Scripts/PlayerMovement.cs
+++ Assets/__Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 12f;
+    public float speed = 2f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -14,10 +14,11 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
 
-    Vector3 velocity;
+    Vector3 _velocity;
     bool isGrounded;
 
     private Animator animator;
+
 
     private void Start()
     {
@@ -26,46 +27,76 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Movement();
+    }
+
+    
+
+    void Movement()
+    {
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y < 0)
+        if (isGrounded && _velocity.y < 0)
         {
-            velocity.y = -2f;
+            _velocity.y = -2f;
         }
 
         float _xMov = Input.GetAxis("Horizontal");
         float _zMov = Input.GetAxis("Vertical");
-        
+
         if (_xMov != 0 || _zMov != 0)
         {
-            animator.SetBool("IsWalking", true);
-        } else
+            // animator.SetBool("IsWalking", true);
+            animator.SetInteger("condition", 1);
+        }
+        else
         {
-            animator.SetBool("IsWalking", false);
+            // animator.SetBool("IsWalking", false);
+            animator.SetInteger("condition", 0);
+
         }
 
+        // Lateral movement
         Vector3 _movHorizontal = transform.right * _xMov;
         Vector3 _movVertical = transform.forward * _zMov;
-
-        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
-
-        controller.Move(_velocity * Time.deltaTime);
-
-        // Vector3 move = transform.right * x + transform.forward * z;
-        // controller.Move(move * speed * Time.deltaTime);
+        Vector3 move = (_movHorizontal + _movVertical).normalized * speed;
+        controller.Move(move * Time.deltaTime);
+        
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            animator.SetTrigger("OnJump");
+            animator.SetBool("in_air", true);
             Invoke("Jump", 0.3f);
         }
+        else
+        {
+            animator.SetBool("in_air", false);
+        }
 
-        velocity.y += 2 * gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        _velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(_velocity * Time.deltaTime);
+
+
+
+    }
+
+    void GetInput()
+    {
+  
+     
+
+
+
     }
 
     void Jump()
     {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        _velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
     }
+
+
+    
 }
