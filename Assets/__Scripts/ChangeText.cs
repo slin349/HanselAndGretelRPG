46e0f90cadcfffadfe017ddb9cf667d49f2520ba
Hans Krohn <hanskrohn70@gmail.com>
2020-03-11 20:53:47 -0400

    Text updates and scenes switch

---
+++ Assets/__Scripts/ChangeText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeText : MonoBehaviour
+{
+    public TextMesh text;
+    public bool currentWeapon;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L)){
+            if(currentWeapon){
+                text.text = "Weapon In Pack";
+                currentWeapon = false;
+            }
+            else{
+                text.text = "Current Weapon";
+                currentWeapon = true;
+            }
+        }
+    }
+    
+}
