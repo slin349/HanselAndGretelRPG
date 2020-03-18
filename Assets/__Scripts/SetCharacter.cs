8e41b39a5a9d0e4134da820c4d8abdea98f65fb0
Hans Krohn <hanskrohn70@gmail.com>
2020-03-18 13:50:43 -0400

    took feet out of the gorund

--- Assets/__Scripts/SetCharacter.cs
+++ Assets/__Scripts/SetCharacter.cs
@@ -16,13 +16,13 @@ public class SetCharacter : MonoBehaviour
             gameObject = Instantiate(character1, parent.transform.position  ,Quaternion.Euler(0f,0f,0f) );
             gameObject.transform.parent = parent.transform;
             gameObject.transform.localScale = new Vector3(1,1,1);
-            gameObject.transform.Translate(0.0f, -1.1f, 0.0f);
+            gameObject.transform.Translate(0.0f, -1.0f, 0.0f);
         }
         else{
             gameObject = Instantiate(character2, parent.transform.position  ,Quaternion.Euler(0f,0f,0f) );
             gameObject.transform.parent = parent.transform;
             gameObject.transform.localScale = new Vector3(1,1,1);
-            gameObject.transform.Translate(0.0f, -1.1f, 0.0f);
+            gameObject.transform.Translate(0.0f, -1.0f, 0.0f);
         }
             
     }
