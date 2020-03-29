f96f4f6eb0d07ded53cc12d3804b5122cb633972
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-03-29 17:14:52 -0400

    Experience prefab script

---
+++ Assets/__Scripts/Experience.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Experience : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
