3ec541cda9adf226f748589510d83a07fc33c6ea
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-03-30 10:39:40 -0400

    Add enemy health class that inherits from player health
    Enemy instantiates exp orbs upon death.

---
+++ Assets/__Scripts/EnemyHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : Health
+{
+    public GameObject expCube;
+
+    override public void TakeDamage(int amount)
+    {
+        currhealth-= amount;
+        if (currhealth <= 0)
+        {
+            print("Died");
+            isDead = true;
+            animator.SetBool("isDead", true);
+            //dropping 4 exp orbs
+            for (int i = 0; i < 4; i++)
+            {
+                Instantiate(expCube, new Vector3(transform.position.x + i, transform.position.y, transform.position.z + 1), transform.rotation);
+            }
+        }
+        else
+        {
+            print("Took damage");
+            animator.SetTrigger("hit");
+        }
+    }
+}
