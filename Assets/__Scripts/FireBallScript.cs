8fcf2ad79bb8d2672dab487ae78e31e965d941d4
Steven Lin <stevenlin159@gmail.com>
2020-03-31 15:01:19 -0400

    Added fireball

---
+++ Assets/__Scripts/FireBallScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallScript : MonoBehaviour
+{
+    public GameObject explosionVFX;
+    public float blastRadius;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destory fireball if collided
+        Destroy(gameObject);
+        //Deploy explosion effects
+        Instantiate(explosionVFX, transform.position, transform.rotation);
+
+        //Deal Damage
+        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+        foreach (Collider nearbyObject in colliders)
+        {
+            Health dealDamage = nearbyObject.GetComponent<Health>();
+
+            //check if nearby objects are not rigidbody
+            if (dealDamage != null)
+            {
+                //add damage when grenade explodes
+                dealDamage.TakeDamage(5);
+            }
+        }
+    }
+}
---
+++ Assets/__Scripts/FireballSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballSpawner : MonoBehaviour
+{
+    public GameObject projectile;
+    public float projectileSpeed;
+    public float timeBeforeDeletion;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            GameObject fireball = Instantiate(projectile, transform) as GameObject;
+            Rigidbody rb = fireball.GetComponent<Rigidbody>();
+
+            rb.velocity = transform.forward * projectileSpeed;
+
+            Destroy(fireball, timeBeforeDeletion);
+        }
+    }
+}
