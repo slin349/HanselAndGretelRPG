50eed1c60e8059414c404731a50ba3879f2dac87
Hans Krohn <hanskrohn70@gmail.com>
2020-03-30 22:19:19 -0400

    Created main menu scene and controll scene. User is currently able to navigate from scene to scene

---
+++ Assets/__Scripts/ControlsPage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlsPage : MonoBehaviour
+{
+    public void PlayScene(){
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void ControlsScene(){
+        SceneManager.LoadScene("ControlsScene");
+    }
+    public void MainScene(){
+        SceneManager.LoadScene("Start");
+    }
+}
